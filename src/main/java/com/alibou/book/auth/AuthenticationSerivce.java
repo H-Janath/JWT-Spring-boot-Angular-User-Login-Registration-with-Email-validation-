84a09852ab2f3e049f077186f77dbe5c18942601
H-Janath <janathhma@gmail.com>
2024-04-14 23:23:29 +0530

    Implement the AuthenticationSerivce class

---
+++ src/main/java/com/alibou/book/auth/AuthenticationSerivce.java
@@ -0,0 +1,68 @@
+package com.alibou.book.auth;
+
+import com.alibou.book.role.RoleRepository;
+import com.alibou.book.user.Token;
+import com.alibou.book.user.TokenRepository;
+import com.alibou.book.user.User;
+import com.alibou.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationSerivce {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenRepository tokenRepository;
+    private UserRepository userRepository;
+    public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName("USER")
+                .orElseThrow(()-> new IllegalArgumentException("ROLE USER was not initialize"));
+        var user = User.builder()
+                .firstname(request.getFirstName())
+                .lastname(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+
+    }
+
+    private Object generateAndSaveActivationToken(User user) {
+        //generat Token
+        String generatedToken = generateAndSaveActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createAt(LocalDateTime.now())
+                .expireAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateAndSaveActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for(int i=0;i<length;i++){
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
+    }
+}
